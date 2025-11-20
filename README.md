3989e1d6f47386ce25b388b0d08f488cc012c089
Paolo Bachicha <bachicha.paolo@gmail.com>
2025-11-20 11:52:48 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# login_auth_bloc_app
+Simmple login-authentication application applying what I've learned so far with BLoC
---
+++ lib/data/repos/auth_repository.dart
@@ -0,0 +1,9 @@
+class AuthRepository {
+  // Authentication methods implemented here
+  Future<void> login(String username, String password) async {
+    await Future.delayed(Duration(seconds: 2));
+
+    
+  }
+
+}
